80cf74b20934b2ac6782c989ce7417155b9121de
KoCaTkAA <135720809+KoCaTkAA@users.noreply.github.com>
2024-09-30 14:08:09 +0300

    delete comments

--- main.py
+++ main.py
@@ -14,10 +14,8 @@ def get_text_messages(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
-    if call.data == "yes": #call.data это callback_data, которую мы указали при объявлении кнопки
-        .... #код сохранения данных, или их обработки
+    if call.data == "yes":
         bot.send_message(call.message.chat.id, 'Запомню : )');
     elif call.data == "no":
-         ... #переспрашиваем
 
 bot.polling(none_stop=True, interval=0)
