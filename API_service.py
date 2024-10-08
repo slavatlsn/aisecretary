2cfc30f38730d2139fc048bbef2251a7aa9fbeab
slavatlsn <93151297+slavatlsn@users.noreply.github.com>
2024-10-08 22:26:39 +0300

    Update API_service.py
    add ability to send messages just testing coze.com API

--- API_service.py
+++ API_service.py
@@ -8,4 +8,16 @@ class Coze():
 
     def create_conversation(self):
         headers = {'Authorization': 'Bearer ' + self.token, 'Content-Type': 'application/json'}
-        print(requests.post('https://api.coze.com/v1/conversation/create', headers=headers))
+        resp = requests.post('https://api.coze.com/v1/conversation/create', headers=headers).json()
+        return resp
+
+    def send_text_message(self, conv_id, text):
+        headers = {'Authorization': 'Bearer ' + self.token, 'Content-Type': 'application/json'}
+        query = {'conversation_id': conv_id} # conv_id - string
+        message = {
+            "role": "user",
+            "content": text,
+            "content_type": "text"
+        }
+        resp = requests.post(' https://api.coze.com/v1/conversation/message/create', headers=headers, params=query, json=message).json()
+        return resp
