de88d0c262e807c4f9f0610399c203d711642ad4
slavatlsn <93151297+slavatlsn@users.noreply.github.com>
2024-10-30 21:50:30 +0300

    Update bot_main.py
    add function about files(нужно с файлами решить проблему с загрузкой)

--- bot_main.py
+++ bot_main.py
@@ -1,17 +1,21 @@
-from cozepy import Coze, TokenAuth, Message, ChatStatus
-import os
+from cozepy import Coze, TokenAuth, Message, ChatStatus, MessageObjectString
+from my_token import token as t
 from pathlib import Path
 
-coze = Coze(auth=TokenAuth(os.getenv("COZE_API_TOKEN")))
-chat_poll = coze.chat.create_and_poll(bot_id='bot_id', user_id='user_id', additional_messages=[Message.build_user_question_text("How are you?")])
+coze = Coze(auth=TokenAuth(t))
+
+#file = coze.files.upload(file=Path('/filepath'))
+s = ''
+
+file = coze.files.upload(file='file.txt')
+print(file)
+chat_poll = coze.chat.create_and_poll(bot_id='7422700799514099718', user_id='0', additional_messages=[Message.build_user_question_text("Сократи текст" + s + 'и выдай ответ в формате *.pdf')])
+#chat_poll = coze.chat.create_and_poll(bot_id='7422700799514099718', user_id='0', additional_messages=[Message.build_user_question_objects(MessageObjectString.build_file())])
+
+
 for message in chat_poll.messages:
     print(message.content, end="")
 
-file = coze.files.upload(file=Path('/filepath'))
-
-coze = Coze(auth=TokenAuth(token))
-
-chat_poll = coze.chat.create_and_poll(bot_id='bot_id', user_id='user_id', additional_messages=[Message.build_user_question_text("Sample message")])
-
-for message in chat_poll.messages:
-    print(message.content, end="")
+if chat_poll.chat.status == ChatStatus.COMPLETED:
+    print()
+    print("token usage:", chat_poll.chat.usage.token_count)
